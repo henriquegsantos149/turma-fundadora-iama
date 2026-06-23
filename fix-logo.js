5ea4d1c7ec58be0b0d819da6071b9232045c9bd1
Henrique Gonzalez <henriquegonzalez@poli.ufrj.br>
2026-06-23 12:10:07 -0300

    security(cleanup): remove all test and temporary script files containing sensitive api credentials

--- fix-logo.js
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';
-let html = fs.readFileSync('index.html', 'utf8');
-html = html.replace('src="/logo-icon.webp"', 'src="/logo.webp"');
-fs.writeFileSync('index.html', html, 'utf8');
--- fix.js
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-const path = 'c:\\\\Users\\\\User\\\\Desktop\\\\Ambiental Pro\\\\PROJETOS\\\\Aplicação IAMA\\\\script.js';
-let content = fs.readFileSync(path, 'utf8');
-content = content.replace(/\\`/g, '`');
-content = content.replace(/\\\$\{/g, '${');
-fs.writeFileSync(path, content, 'utf8');
